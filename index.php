a5897e3c60deeadfffcd177f4040aca345c92a55
LukeInJapan <LukeInJapan@users.noreply.github.com>
2018-06-03 18:16:57 +0900

    Test

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+
+	<title>
+		<?php bloginfo('name');?>
+	</title>
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width=device-width">
+	<?php wp_head();?>
+
+</head>
+<body <?php body_class();?>>
+<!-- Site header -->
+	<div class="header">
+	<header>
+		<h1 class="headerPosition"><a class="headerTitle" href="<?php echo home_url();?>"><?php bloginfo('name');?></a></h1>
+		<h5 class="headerSlogan"><?php bloginfo('description');?></h5>
+	</header>
+	<nav>Test</nav>
+	</div>
+	<div class="mainBody">
+<!-- Grid container -->
+	<div class="gridContainer">
+		<div style="background-color: blue;">
+		<!-- start slipsum code -->
+
+		<h4>We happy?</h4>
+		<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+
+		<h4>Is she dead, yes or no?</h4>
+		<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+
+		<h4>I gotta piss</h4>
+		<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
+
+		<h4>Are you ready for the truth?</h4>
+		<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+
+		<h4>No man, I don't eat pork</h4>
+		<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+
+		<h4>I'm serious as a heart attack</h4>
+		<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+
+		<h4>We happy?</h4>
+		<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+
+		<h4>I gotta piss</h4>
+		<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+
+		<h4>Uuummmm, this is a tasty burger!</h4>
+		<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+
+		<h4>I gotta piss</h4>
+		<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+
+		<h4>No, motherfucker</h4>
+		<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+
+		<h4>Are you ready for the truth?</h4>
+		<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+
+		<h4>Are you ready for the truth?</h4>
+		<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
+
+		<h4>Are you ready for the truth?</h4>
+		<p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+
+		<h4>Hold on to your butts</h4>
+		<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+
+		<!-- end slipsum code -->
+
+
+<!-- Nested grid -->
+	</div>
+		<div class="nestedGridContainer" style="background-color: green;">
+			<div style="background-color: yellow;">Test 3</div>
+			<div style="background-color: purple;">Test 4</div>
+		</div>
+	</div>
+<!-- End Grid Container div -->
+	</div>
+<!-- Footer div -->
+	<div class="footer">
+	<p><?php echo bloginfo('name');?> - &copy; <?php echo date('Y');?></p>
+</div>
+</body>
+</html>
